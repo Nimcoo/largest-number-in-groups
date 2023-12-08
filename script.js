710c16ebd5631b2a86bf14774df40a35bf970f66
NIMCOO <nimco77001@gmail.com>
2023-12-08 08:00:20 +0000

    Done Exercise

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 // Good Luck 💪🏾
+function givenNumber(Array){
+    let LargestNumbers = Array[0];
+    for (let i = 0; i < Array.lengt; i++) {
+        if (Array[i] > LargestNumbers)
+        LargestNumbers = Array[i];
+  
+    }
+    return LargestNumbers;
+}
+const myArray =givenNumber([[7, 3, 8, 11], [54, 12, 43, 77], [16, 11, 8]]) 
+console.log(myArray)
+
+//findLargestNumbers([[22, 55, 12], [45, 66, 95],
+
+function givenNumber(Array){
+    let LargestNumbers = Array[0];
+    for (let i = 0; i < Array.lengt; i++) {
+        if (Array[i] > LargestNumbers)
+        LargestNumbers = Array[i];
+  
+    }
+    return LargestNumbers;
+}
+const myarray =givenNumber([[22, 55, 12], [45, 66, 95], [22, 5, 11]]) 
+
+console.log(myarray)
+
+
+
